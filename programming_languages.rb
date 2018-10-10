aaec682139016454e9477215cd02d59c3214ef25
Kelly Sheddy <jungaka07@gmail.com>
2018-10-10 22:00:09 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,6 +1,6 @@
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, value|
-    value.each do |form, type|
-      if new_hash.has_key? ()
+    value.each do |name, type|
+      if new_hash.has_key? (name)
 end
